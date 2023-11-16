3ddbc7f912e0d10153506640c09aa0c869b26b14
jylee0805 <smexoshinee17@gmail.com>
2023-11-16 13:24:05 +0800

    Initial commit

---
+++ js/all.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+    $('.showmenu').on('click',function(e){
+        e.preventDefault();
+        $('body').toggleClass('menu-show');
+    });
+    $('.btn1').click(function(){
+        $('.dt1').slideToggle(500);
+    });
+    $('.btn2').click(function(){
+        $('.dt2').slideToggle(500);
+    });
+    $('.btn3').click(function(){
+        $('.dt3').slideToggle(500);
+    });
+    $('.btn4').click(function(){
+        $('.dt4').slideToggle(500);
+    });
+});
